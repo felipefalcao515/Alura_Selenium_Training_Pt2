29c892bd5eced16c69e07b421c6ad13733a245a5
felipefalcao515 <felipe.falcao515@gmail.com>
2024-04-26 16:03:29 -0300

    Importei o projeto do Alura e criei um teste logout

---
+++ Alura.LeilaoOnline.Selenium/PageObjects/DashboardInteressadaPO.cs
@@ -0,0 +1,38 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+
+namespace Alura.LeilaoOnline.Selenium.PageObjects
+{
+    public class DashboardInteressadaPO
+    {
+        private IWebDriver driver;
+        private By byLogoutLink; //Define uma variavel privada para o logout, para proteger o valor.
+        private By byMeuPerfilLink;
+
+        public DashboardInteressadaPO(IWebDriver driver)
+        {
+            this.driver = driver;
+            byLogoutLink = By.Id("logout"); //Define o que é a variavel privada de logout.
+            byMeuPerfilLink = By.Id("meu-perfil");
+        }
+
+        public void EfetuarLogout()
+        {
+            var linkMeuPerfil = driver.FindElement(byMeuPerfilLink);
+            var linkLogout = driver.FindElement(byLogoutLink); //Encontra o elemento de logout usando a variavel privada.
+
+            //Simula a ação de um usuario usando a interface Interactions do Selenium.
+            IAction acaoLogout = new Actions(driver)
+                //Mover para o elemento meu-perfil.
+                .MoveToElement(linkMeuPerfil)
+                //Mover para o link de logout.
+                .MoveToElement(linkLogout)
+                //Clicar no link de logout.
+                .Click()
+                .Build();
+
+            //Executa o IAction.
+            acaoLogout.Perform();
+        }
+    }
+}
---
+++ Alura.LeilaoOnline.Selenium/Testes/AoEfetuarLogout.cs
@@ -0,0 +1,35 @@
+﻿using Xunit;
+using OpenQA.Selenium;
+using Alura.LeilaoOnline.Selenium.Fixtures;
+using Alura.LeilaoOnline.Selenium.PageObjects;
+
+namespace Alura.LeilaoOnline.Selenium.Testes
+{
+    [Collection("Chrome Driver")]
+    public class AoEfetuarLogout
+    {
+        private IWebDriver driver;
+
+        public AoEfetuarLogout(TestFixture testFixture)
+        {
+            driver = testFixture.Driver;
+        }
+
+        [Fact]
+        public void DadoLoginValidoDeveIrParaHomeNaoLogada()
+        {
+            //arrange
+            var loginPO = new LoginPO(driver); //Importa a classe LoginPO da pasta PageObjects.
+            //Importa os metodos da classe LoginPO
+            loginPO.Visitar();
+            loginPO.PreencheFormulario("fulano@example.org", "123");
+            loginPO.SubmeteFormulario();
+
+            var dashboardPO = new DashboardInteressadaPO(driver);
+            //act
+            dashboardPO.EfetuarLogout();
+            //assert
+            Assert.Contains("Próximos Leiloes", driver.PageSource);
+        }
+    }
+}
