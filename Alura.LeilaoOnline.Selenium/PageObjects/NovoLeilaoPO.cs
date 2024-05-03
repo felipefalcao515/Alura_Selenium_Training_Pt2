cfdd526c812fbffe37dc909da43899e7f52c878a
felipefalcao515 <felipe.falcao515@gmail.com>
2024-05-03 16:31:22 -0300

    Teste Criando Novo Leilão
    O professor introduziu a criação de um Novo Leilão, o processo de criação envolve o preenchimento de um formulário complexo. Comecei a criação do PO para o teste, defini um localizador para cada "input" usando os "id" respectivos. Introduzi um arquivo de teste para importar o PO do formulario, também comecei a escrita do teste até a pagina de login.

---
+++ Alura.LeilaoOnline.Selenium/PageObjects/NovoLeilaoPO.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using OpenQA.Selenium;
+
+
+namespace Alura.LeilaoOnline.Selenium.PageObjects
+{
+    public class NovoLeilaoPO
+    {
+        private IWebDriver driver;
+
+        //Elementos do formulario.
+        private By byInputTitulo;
+        private By byInputDescricao;
+        private By byInputCategoria;
+        private By byInputValorInicial;
+        private By byInputImagem;
+        private By byInputInicioPregao;
+        private By byInputTerminoPregao;
+
+        public NovoLeilaoPO(IWebDriver driver)
+        {
+            this.driver = driver;
+            //Licalizador dos elementos do formulario.
+            byInputTitulo = By.Id("Titulo");
+            byInputDescricao = By.Id("Descricao");
+            byInputCategoria = By.Id("Categoria");
+            byInputValorInicial = By.Id("ValorInicial");
+            byInputImagem = By.Id("Imagem");
+            byInputInicioPregao = By.Id("InicioPregao");
+            byInputTerminoPregao = By.Id("TerminoPregao");
+        }
+
+        public void Visitar()
+        {
+            driver.Navigate().GoToUrl("https://localhost:5000/Leloes/Novo");
+        }
+
+        public void PreencherFormulario(
+            string titulo,
+            string descricao,
+            string categoria,
+            double valor,
+            string imagem,
+            DateTime inicio,
+            DateTime termino
+            )
+        {
+            driver.FindElement(byInputTitulo).SendKeys(titulo);
+            driver.FindElement(byInputDescricao).SendKeys(descricao);
+            driver.FindElement(byInputCategoria).SendKeys(categoria);
+            driver.FindElement(byInputValorInicial).SendKeys(valor.ToString());
+            driver.FindElement(byInputImagem).SendKeys(imagem);
+            driver.FindElement(byInputInicioPregao).SendKeys(inicio.ToString());
+            driver.FindElement(byInputTerminoPregao).SendKeys(termino.ToString());
+        }
+    }
+}
---
+++ Alura.LeilaoOnline.Selenium/Testes/AoCriarLeilao.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using OpenQA.Selenium;
+using Xunit;
+using Alura.LeilaoOnline.Selenium.PageObjects;
+using Alura.LeilaoOnline.Selenium.Fixtures;
+
+
+namespace Alura.LeilaoOnline.Selenium.Testes
+{
+    [Collection("Chrome Driver")]
+    public  class AoCriarLeilao
+    {
+        private IWebDriver driver;
+
+        public AoCriarLeilao(TestFixture fixture)
+        {
+            driver = fixture.Driver;
+        }
+
+        [Fact]
+        public void DadoLoginAdministratoEInfoValidasDeveCadastrarLeilao()
+        {
+            //arrange
+            var LoginPO = new LoginPO(driver);
+            LoginPO.Visitar();
+            LoginPO.PreencheFormulario("admin@example.org","123");
+            LoginPO.SubmeteFormulario();
+
+            
+            //action
+            //assert
+        }
+    }
+}
