a7f68ea19f335d08cea0956207bcd0f189f14cc6
felipefalcao515 <felipe.falcao515@gmail.com>
2024-05-07 16:11:32 -0300

    Finished implement test for a new auction
    Included locators for each field, expected attribute for each fiel, value for each expected attribute, a css rule for the submit button, as well as a simple assertion(might improve later)

--- Alura.LeilaoOnline.Selenium/PageObjects/NovoLeilaoPO.cs
+++ Alura.LeilaoOnline.Selenium/PageObjects/NovoLeilaoPO.cs
@@ -16,6 +16,7 @@ namespace Alura.LeilaoOnline.Selenium.PageObjects
         private By byInputImagem;
         private By byInputInicioPregao;
         private By byInputTerminoPregao;
+        private By byBotaoSalvar;
 
         public NovoLeilaoPO(IWebDriver driver)
         {
@@ -25,9 +26,10 @@ namespace Alura.LeilaoOnline.Selenium.PageObjects
             byInputDescricao = By.Id("Descricao");
             byInputCategoria = By.Id("Categoria");
             byInputValorInicial = By.Id("ValorInicial");
-            byInputImagem = By.Id("Imagem");
+            byInputImagem = By.Id("ArquivoImagem");
             byInputInicioPregao = By.Id("InicioPregao");
             byInputTerminoPregao = By.Id("TerminoPregao");
+            byBotaoSalvar = By.CssSelector("button [type=submit]");
         }
 
         public void Visitar()
@@ -36,22 +38,27 @@ namespace Alura.LeilaoOnline.Selenium.PageObjects
         }
 
         public void PreencherFormulario(
-            string titulo,
-            string descricao,
-            string categoria,
-            double valor,
-            string imagem,
-            DateTime inicio,
-            DateTime termino
-            )
+            string titulo, // Nome do leilão.
+            string descricao, // Descrição do Leilão.
+            string categoria, // Categoria do Leilão.
+            double valor,   // Valor do Leilão.
+            string imagem,  // Caminho da imagem, no caso local.
+            DateTime inicio, // Data inicio.
+            DateTime termino    // Data fim.
+            ) // Define os valores para serem inseridos.
         {
             driver.FindElement(byInputTitulo).SendKeys(titulo);
             driver.FindElement(byInputDescricao).SendKeys(descricao);
             driver.FindElement(byInputCategoria).SendKeys(categoria);
             driver.FindElement(byInputValorInicial).SendKeys(valor.ToString());
             driver.FindElement(byInputImagem).SendKeys(imagem);
-            driver.FindElement(byInputInicioPregao).SendKeys(inicio.ToString());
-            driver.FindElement(byInputTerminoPregao).SendKeys(termino.ToString());
+            driver.FindElement(byInputInicioPregao).SendKeys(inicio.ToString("dd/mm/yyyy")); //Usa o sendkeys para inserir a data do pregão, convertendo o que seria um númerico para string.
+            driver.FindElement(byInputTerminoPregao).SendKeys(termino.ToString("dd/mm/yyyy")); //Usa o sendkeys para inserir a data do pregão, convertendo o que seria um númerico para string.
+        }
+
+        public void SubmeteFormulario()
+        {
+            driver.FindElement(byBotaoSalvar).Click();
         }
     }
 }
--- Alura.LeilaoOnline.Selenium/Testes/AoCriarLeilao.cs
+++ Alura.LeilaoOnline.Selenium/Testes/AoCriarLeilao.cs
@@ -3,6 +3,7 @@ using OpenQA.Selenium;
 using Xunit;
 using Alura.LeilaoOnline.Selenium.PageObjects;
 using Alura.LeilaoOnline.Selenium.Fixtures;
+using System.Threading;
 
 
 namespace Alura.LeilaoOnline.Selenium.Testes
@@ -26,9 +27,23 @@ namespace Alura.LeilaoOnline.Selenium.Testes
             LoginPO.PreencheFormulario("admin@example.org","123");
             LoginPO.SubmeteFormulario();
 
-            
-            //action
+            var novoLeilaoPo = new NovoLeilaoPO(driver);
+            novoLeilaoPo.Visitar();
+            novoLeilaoPo.PreencherFormulario(
+                "Leilão de Coleção 1", // Nome do leilão.
+                "Nullam aliquet condimentum elit vitae volutpat. Vivamus ut nisi venenatis, facilisis odio eget, lobortis tortor. Cras mattis sit amet dolor id bibendum. Nulla turpis justo, porttitor eget leo vel, dictum tempor diam. Sed dui arcu, feugiat nec placerat ac, suscipit a mi. Etiam eget risus et tellus placerat tincidunt at ut lorem.",
+                "Item de Colecionador",
+                4000, // Valor R$ do leilão.
+                "c:\\imagens\\colecao1.jpg", // Caminho da imagem no armazenamento local.
+                DateTime.Now.AddDays(20), // Valor de data inicio do pregão.
+                DateTime.Now.AddDays(40) // Valor de data fim do pregão.
+                );
+            //act
+            novoLeilaoPo.SubmeteFormulario();
             //assert
+            Assert.Contains("Leilões cadastrados no sistema", driver.PageSource);
+
+            Thread.Sleep(5000);
         }
     }
 }
